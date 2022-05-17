b85c5441188096e0a5761edb56c76684fdc47b03
Pitta <arthur_pitta@yahoo.com.br>
2022-05-17 13:55:33 -0300

    Hot fix to heroku staticfiles

--- project/settings.py
+++ project/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
@@ -137,12 +137,11 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = ['django.contrib.staticfiles.storage.StaticFilesStorage',
-                    'whitenoise.storage.CompressedManifestStaticFilesStorage']
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media (images and videos)
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
