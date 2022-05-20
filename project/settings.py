4de29866b077b62885559efdc3d82e56b808e32d
Pitta <arthur_pitta@yahoo.com.br>
2022-05-20 14:46:12 -0300

    Add lack information in home page

--- project/settings.py
+++ project/settings.py
@@ -19,7 +19,6 @@ env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-print(BASE_DIR)
 
 # Take environment variable form .env file
 environ.Env.read_env(os.path.join(BASE_DIR, 'project/.env'))
